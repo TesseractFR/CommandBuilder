664545b573a17cc2686f314a0f112ae734108efe
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2021-11-02 19:53:29 +0100

    clean: remove unused imports

--- src/main/java/onl/tesseract/commandBuilder/CommandBuilder.java
+++ src/main/java/onl/tesseract/commandBuilder/CommandBuilder.java
@@ -1,8 +1,5 @@
 package onl.tesseract.commandBuilder;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
